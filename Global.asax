601f94b60d84dedbd666950f57f9065261472184
KonstantinLupov <arebando10@abv.bg>
2016-12-05 18:45:06 +0200

    Blank MVC Project

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="C_Sharp_Project.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(C_Sharp_Project.Startup))]
+namespace C_Sharp_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
